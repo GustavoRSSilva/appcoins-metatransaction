b7816e9c529eae6d60f02fa5c010514d5e86aeba
Gustavo Silva <gustavo.silva@aptoide.com>
2018-11-15 17:30:14 +0000

    [new] started creating raw transactions

--- ui/src/containers/Footer/index.js
+++ ui/src/containers/Footer/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 function Footer() {
+    // <h1>Server</h1>
+    // <h4>From your xprv, for m/44'/60'/0'/0/0, generated:</h4>
+    // <p>Private key = 0x8418aae96ffc26ec190e8e198319f20c78de9ab533c18bd9a623c024eac23813</p>
+    // <p>Public key = 0x03efa378062096e281791de45e0005a0b9460d615c9a64029c8a5177c12feebe26</p>
+    // <p>Address = 0x1DD02B96E9D55E16c646d2F21CA93A705ac667Bf</p>
   return (
       <footer>
-
+          <h1>Client</h1>
+          <h4>Mnemonic: slab piece until prison artist seed pool attack fortune isolate fitness myth portion leisure ghost</h4>
           <h4>From your xprv, for m/44'/60'/0'/0/0, generated:</h4>
-          <p>Private key = 0x8418aae96ffc26ec190e8e198319f20c78de9ab533c18bd9a623c024eac23813</p>
-          <p>Public key = 0x03efa378062096e281791de45e0005a0b9460d615c9a64029c8a5177c12feebe26</p>
-          <p>Address = 0x1DD02B96E9D55E16c646d2F21CA93A705ac667Bf</p>
+          <p>Private key = 0xd8508c00623c69e12a41d27a786e5fe51c1e4cb632487d496e78a581d13f5d0a</p>
+          <p>Public key = 0x034189e80724c8fbb2fd1f61b9902e91a30de6debcfcdcd651d6a1d96a6c65ae3c</p>
+          <p>Address = 0x3c63116E7594CFeBD5B950352Fc89DF21c1C5F9e</p>
+
 
           *for more information see: <a href="https://iancoleman.io/bip39/" target="_blank">https://iancoleman.io/bip39/</a>
 
